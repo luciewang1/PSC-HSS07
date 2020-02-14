a8fa23504bac87aaf66d1abbcdcc86622a9f1672
luciewang1 <56896800+luciewang1@users.noreply.github.com>
2020-02-14 17:33:47 +0100

    Add file via upload

---
+++ Traitement données/SeqAnalysis.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script to quickly analyze the reaction time depending on past stimuli.
+    - import data
+    - compute the average RT depending on preceding sequences.
+    - plot the corresponding graph.
+@author: Lucie Wang
+"""
+
+import os
+from utils import import_one_subject, get_serie_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Set parameters
+
+is_RT = True # analyse du RT ou de l'erreur
+
+## Import all data
+
+data = [None]*3 # data[session][subject] : data for given subject of a given session
+
+is_active = [None]*3 # is_active[session][subject] : boolean whether a given subject of a given session counts in our data
+
+for session in range(3):
+    data[session] = [None]*20
+    is_active[session] = [False]*20
+
+    rootdir = "../Données/Session " + str(session+1)
+
+    for file in os.listdir(rootdir):
+        if file.endswith(".xpd"):
+            subj = int(file.split("_")[1])-1
+            data[session][subj] = import_one_subject(rootdir, file)
+            is_active[session][subj] = True
+
+## Helper functions
+
+def int_of_seq(seq): # encode sequences of order k in range(2^k), by reverse binary reading
+    k = len(seq)
+    n = 0
+    for i in range(k):
+        n += seq[i]*(2**i)
+    return n
+
+def seq_of_int(n, k): # decode sequences of order k from range(2^k), by reverse binary writing
+    num = n
+    seq = [0]*k
+    for i in range(k):
+        if num%2:
+            seq[i] = 1
+        num //= 2
+    return seq
+
+def str_of_seq(seq): # write sequence as a string of A's and R's
+    return ''.join(list(map(lambda x: 'R' if x else 'A', seq)))
+
+def int_of_str(s):
+    if s == 'True':
+        return 1
+    return 0
+
+## Compute average RT depending on sequence
+
+def init(k, exp_type=None): # k : history order, exp_type = 0 (motricity) or 1 (timing)
+    N = 2**k
+    if exp_type == None: # RT[n] : [sum of RTs, number of occurrences] for a given sequence
+        RT = [None]*N
+        for n in range(N):
+            RT[n] = [0,0]
+        return RT
+    else: # RT[ecc][param][n] : [sum of RTs, number of occurrences] for a given sequence and imposed block parameters
+        RT = [None]*3
+        for ecc in range(3):
+            RT[ecc] = [None]*2
+            for param in range(2):
+                RT[ecc][param] = [None] * N
+                for n in range(N):
+                    RT[ecc][param][n] = [0,0]
+        return RT
+
+def add_subject(dat, RT, k, exp_type=None):
+    for block_num in range(6):
+        ecc = int(get_serie_data(dat, 'ecc', block_num)[0])
+        if exp_type == 1:
+            param = int(get_serie_data(dat, 'delay', block_num)[0])
+        if exp_type == 0:
+            param = int(get_serie_data(dat, 'motor', block_num)[0])
+        for serie_id in range(2):
+            serie_rep = get_serie_data(dat, 'rep', block_num, serie_id)
+            serie_RT = get_serie_data(dat, 'RT', block_num, serie_id)
+            for trial in range(k, len(serie_RT)):
+                n = int_of_seq(list(map(int_of_str, serie_rep[trial - k + 1:trial + 1])))
+                if serie_RT[trial] and serie_RT[trial] > 0: # cases excluded : RT = None and RT < 0
+                    if exp_type == None:
+                        RT[n][0] += float(serie_RT[trial])
+                        RT[n][1] += 1
+                    else:
+                        RT[ecc][param][n][0] += float(serie_RT[trial])
+                        RT[ecc][param][n][1] += 1
+    return RT
+
+def build_RT(k, exp_type=None): # RT[n] : average reaction time for a given sequence
+    N = 2**k
+    RT = init(k, exp_type)
+    if exp_type == None:
+        for session in range(3):
+            for subj in range(20):
+                if is_active[session][subj]:
+                    RT = add_subject(data[session][subj], RT, k)
+        for n in range(N):
+            RT[n] = RT[n][0]/RT[n][1] if RT[n][1] else 0
+    else:
+        for session in range(3):
+            for subj in range(20):
+                if is_active[session][subj]:
+                    RT = add_subject(data[session][subj], RT, k, exp_type)
+        for ecc in range(3):
+            for param in range(2):
+                for n in range(N):
+                    RT[ecc][param][n] = RT[ecc][param][n][0]/RT[ecc][param][n][1] if RT[ecc][param][n][1] else 0
+    return RT
+
+## Plot average RT depending on sequence
+
+def plot_RT(k, exp_type=None):
+    N = 2**k
+    x = [i for i in range(N)]
+    RT = build_RT(k, exp_type)
+    if exp_type == None:
+        plt.plot(x, RT, "o-")
+    else:
+        for ecc in range(3):
+            for param in range(2):
+                plt.plot(x, RT[ecc][param], "o-", label='ecc = ' + str(ecc) + ", " + ("delay" if exp_type else "mot") + " = " + str(param))
+    plt.xticks(x, [str_of_seq(seq_of_int(i, k)) for i in range(N)], rotation=60, fontsize='small')
+    plt.ylabel("Reaction time (ms)")
+    plt.title("Reaction time depending on " + str(k) + " last stimuli" + (", in " + ("timing" if exp_type else "motricity") + " experiment" if exp_type != None else ""))
+    plt.legend()
+    plt.show()
+plot_RT(4, exp_type=1)
