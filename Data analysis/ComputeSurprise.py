Correct formula for surprise
-log(p) instead of p

Data analysis/ComputeSurprise.py
@@ -12,19 +12,20 @@ from MarkovModel_Python import GenerateSequence as sg
 
 def add_surprise(dat, decay=16, order=1):
     """
-    Add theoretical surprise to individual data, according to the Minimal Transition Probabilities Model reset at every new series.
+    Add theoretical surprise (-log(prob)) to individual data, according to the Minimal Transition Probabilities Model reset at every new series.
     Input: data dictionary (by attribute) of lists (by trial) for one subject, decay parameter for the model, order for the model.
     Output: same data dictionary updated.
     """
 
     options = {'Decay': decay}
     seq = dat["seq"]
-    surprise = [] # list for theoretical surprises
+    prob = [] # list for probabilities
     for seriesId in range(12):
         seqSeries = seq[seriesId*120:(seriesId+1)*120]
         seqSeries = sg.ConvertSequence(seqSeries)['seq']  # seq : stimuli sequence (1/0 for square/disk)
 
         out_fixed = IO.IdealObserver(seqSeries, 'fixed', order=order, options=options)
-        surprise += list(out_fixed['surprise'])
+        prob += list(out_fixed['surprise'])
+    surprise = list(map(lambda p: -np.log(p), prob)) # list for theoretical surprises
     dat["surprise"] = surprise
     return dat